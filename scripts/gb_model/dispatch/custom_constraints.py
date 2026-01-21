ed978e9f18a031733b094e90ab8c3c1db4a372c5
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2026-01-21 08:20:46 +0000

    Fix dispatch custom constraints script method names

--- scripts/gb_model/dispatch/custom_constraints.py
+++ scripts/gb_model/dispatch/custom_constraints.py
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: MIT
 import logging
 
+import pandas as pd
+import pypsa
+from snakemake.script import Snakemake
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,3 +16,20 @@ def remove_KVL_constraints(n, snapshots, snakemake):
     """
     n.model.remove_constraints("Kirchhoff-Voltage-Law")
     logger.info("Remove KVL constraints from the model")
+
+
+def custom_constraints(
+    n: pypsa.Network,
+    snapshots: pd.Index,
+    snakemake: Snakemake,
+) -> None:
+    """
+    Apply custom constraints to the PyPSA network.
+
+    Args:
+        n (pypsa.Network): The PyPSA network to modify.
+        snapshots (pd.Index): The snapshots of the network.
+        snakemake (snakemake.Snakemake): The snakemake object for parameters and config.
+    """
+    # Apply boundary constraints
+    remove_KVL_constraints(n, snapshots, snakemake)
