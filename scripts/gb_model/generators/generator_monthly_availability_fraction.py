8664962af04d14e78cc3fbb7d2fd3e12ccd46525
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2026-02-03 18:20:47 +0000

    Update outage data calcs to use entsoe-py and dukes

--- scripts/gb_model/generators/generator_monthly_availability_fraction.py
+++ scripts/gb_model/generators/generator_monthly_availability_fraction.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import pandas as pd
 
 from scripts._helpers import configure_logging, set_scenario_config
+from scripts.gb_model.generators import create_powerplants_table
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +23,6 @@ logger = logging.getLogger(__name__)
 def prep_outage_data(
     input_path: str,
     max_unavailable_days: int,
-    resource_type_mapping: dict,
     carrier_mapping: dict,
 ) -> pd.DataFrame:
     """
@@ -37,47 +37,39 @@ def prep_outage_data(
     Returns:
         pd.DataFrame: A DataFrame containing the prepared outage data.
     """
-    outages = pd.read_csv(input_path, parse_dates=["start_time", "end_time"])
-    outages = outages[
-        (outages.end_time - outages.start_time).dt.days <= max_unavailable_days
-    ]
+    outages = pd.read_csv(input_path, dtype={"nominal_power": int, "qty_avail": int})
+    for dt_col in ["start", "end"]:
+        outages[dt_col] = pd.to_datetime(outages[dt_col], utc=True)
 
-    for mapping in ["Fueltype", "Technology"]:
-        outages[mapping] = outages["resource_type"].map(resource_type_mapping[mapping])
-    outages["carrier"] = (
-        outages["Fueltype"].fillna(outages["Technology"]).map(carrier_mapping)
-    )
+    outages = outages[(outages.end - outages.start).dt.days <= max_unavailable_days]
+
+    outages["carrier"] = outages["plant_type"].map(carrier_mapping)
 
     outages = outages.dropna(subset=["carrier"])
     if outages.empty:
         logger.warning(f"No outages found in {input_path} after filtering")
-        return outages
+    elif (isna := outages["carrier"].isnull()).any():
+        missing_carriers = outages[isna]["plant_type"].unique()
+        logger.warning(
+            f"Some plant types in {input_path} could not be mapped to carriers: {missing_carriers}"
+        )
+    outages["max_unavailable_mw"] = outages["nominal_power"] - outages["avail_qty"]
 
     return outages
 
 
-def _start_end_to_month_series(series: pd.Series) -> pd.Series:
+def _add_outages_to_ts(series: pd.Series, sum_df: pd.Series) -> None:
     """
-    Convert start and end time to a series of months.
+    Add outage data to a time series based on start and end times.
 
     Args:
         series (pd.Series): A Series containing start and end times.
-
-    Returns:
-        pd.Series: A Series indexed by month with the maximum unavailable MW for each month.
+        sum_df (pd.Series): An 1-minute resolution timeseries to accumulate the maximum unavailable MW.
     """
-    full_date_range = pd.date_range(
-        start=f"{snakemake.params.start_date} 00:00",
-        end=f"{snakemake.params.end_date} 23:59",
-        freq="min",
-        tz=series["start_time"].tz,
+
+    sum_df.loc[slice(series["start"].round("min"), series["end"].round("min"))] += (
+        series["max_unavailable_mw"]
     )
-    new_df = pd.Series(0, index=full_date_range, dtype=float)
-    new_df.loc[
-        slice(series["start_time"].round("min"), series["end_time"].round("min"))
-    ] = series["max_unavailable_mw"]
-    monthly_df = new_df.groupby(new_df.index.month).mean()
-    return monthly_df
 
 
 def monthly_outages(df: pd.DataFrame, carrier_max_cap: pd.Series) -> pd.DataFrame:
@@ -91,9 +83,18 @@ def monthly_outages(df: pd.DataFrame, carrier_max_cap: pd.Series) -> pd.DataFram
     Returns:
         pd.DataFrame: DataFrame containing monthly outages as a fraction of total capacity.
     """
-    monthly_outages = df.apply(_start_end_to_month_series, axis=1)
+    full_date_range = pd.date_range(
+        start=f"{snakemake.params.start_date} 00:00",
+        end=f"{snakemake.params.end_date} 23:59",
+        freq="min",
+        tz=df["start"].dt.tz,
+    )
+    sum_df = pd.Series(0, index=full_date_range, dtype=float)
+    df.apply(_add_outages_to_ts, axis=1, sum_df=sum_df)
     total_cap = carrier_max_cap.loc[df.name]
-    availability_fraction = (total_cap - monthly_outages.sum()) / total_cap
+    availability_fraction = (
+        total_cap - sum_df.groupby(sum_df.index.month).mean()
+    ) / total_cap
     logger.info(
         f"Calculated monthly {df.name} availability from {len(df)} outage events as a fraction of total capacity. "
         f"Average annual unavailability: {availability_fraction.mean()}"
@@ -109,32 +110,29 @@ if __name__ == "__main__":
     configure_logging(snakemake)
     set_scenario_config(snakemake)
 
-    powerplants = pd.read_csv(snakemake.input.powerplants)
-    all_outages = pd.concat(
-        [
-            prep_outage_data(
-                snakemake.input[i],
-                snakemake.params.max_unavailable_days,
-                snakemake.params.resource_type_mapping,
-                snakemake.params.carrier_mapping,
-            )
-            for i in ["planned", "forced"]
-        ]
+    all_outages = prep_outage_data(
+        snakemake.input.outages,
+        snakemake.params.max_unavailable_days,
+        snakemake.params.entsoe_carrier_mapping,
     )
     logger.info(f"Loaded {len(all_outages)} outage records")
 
-    powerplants_carriers = (
-        powerplants["Fueltype"]
-        .map(snakemake.params.carrier_mapping)
-        .fillna(powerplants["Technology"].map(snakemake.params.carrier_mapping))
+    df_dukes = pd.read_csv(snakemake.input.dukes_data)
+    dukes_config = snakemake.params.dukes_config
+    default_set = snakemake.params.default_set
+    df_capacity_gb_dukes = create_powerplants_table.capacity_table(
+        df_dukes, dukes_config, default_set
     )
-    carrier_max_cap = (
-        powerplants[powerplants.Country == "GB"]
-        .groupby(powerplants_carriers)
-        .Capacity.sum()
-    )
-
-    grouped_outages = all_outages.groupby("carrier").apply(
+    carrier_max_cap = df_capacity_gb_dukes.groupby("carrier")["p_nom"].sum()
+    missing_carriers = set(all_outages["carrier"]) - set(carrier_max_cap.index)
+    if missing_carriers:
+        logger.warning(
+            f"The following carriers have outage data but no capacity data and will be skipped: {missing_carriers}"
+        )
+    all_outages_filtered = all_outages[
+        all_outages["carrier"].isin(carrier_max_cap.index)
+    ]
+    grouped_outages = all_outages_filtered.groupby("carrier").apply(
         monthly_outages,
         include_groups=False,
         carrier_max_cap=carrier_max_cap,
