19e3b3559edc03c3de10ca572db3921e12a09eaa
Sermisha Narayana <sermisha.narayana@openenergytransition.org>
2026-02-13 10:58:14 +0530

    code: Align code for calculating marginal costs

--- scripts/gb_model/redispatch/calculate_bid_offer_multipliers.py
+++ scripts/gb_model/redispatch/calculate_bid_offer_multipliers.py
@@ -17,12 +17,13 @@ from scripts.gb_model.generators.assign_costs import (
     _load_costs,
     _load_fes_carbon_costs,
     _load_fes_power_costs,
+    calculate_marginal_costs,
 )
 
 logger = logging.getLogger(__name__)
 
 
-def calculate_marginal_costs(
+def calculate_costs(
     fes_power_costs_path: str,
     fes_carbon_costs_path: str,
     fes_scenario: str,
@@ -76,35 +77,22 @@ def calculate_marginal_costs(
         names=["carrier", "year"],
     )
     df_tech = pd.DataFrame(index=multi_index)
+
+    # Merge FES power costs
     df_tech = df_tech.join(fes_power_costs.reset_index().set_index(["carrier", "year"]))
-    df_tech = df_tech.join(fes_carbon_costs, on="year")
+
+    # Merge technology costs data
     df_tech = (
         df_tech.reset_index()
         .set_index("carrier")
         .join(costs[["efficiency", "CO2 intensity"]])
-    )
+    ).reset_index()
 
-    # Fill missing values for fuel and CO2 intensity
-    for tech in df_tech.index.unique().tolist():
-        for param in ["CO2 intensity", "fuel"]:
-            if (
-                tech in costs_config["carrier_gap_filling"][param].keys()
-                and df_tech.at[tech, param].isna().any()
-            ):
-                df_tech.at[tech, param] = costs.at[
-                    costs_config["carrier_gap_filling"][param][tech], param
-                ]
-
-    # Fill any missing values with default characteristics from the config
-    df_tech[costs_config["marginal_cost_columns"]] = df_tech[
-        costs_config["marginal_cost_columns"]
-    ].apply(lambda x: x.fillna(costs_config["default_characteristics"][x.name]["data"]))
-
-    # Calculate marginal costs
-    df_tech["marginal_cost"] = (
-        df_tech["VOM"]
-        + df_tech["fuel"] / df_tech["efficiency"]
-        + df_tech["CO2 intensity"] * df_tech["carbon_cost"] / df_tech["efficiency"]
+    df_tech = calculate_marginal_costs(
+        df_tech,
+        costs,
+        costs_config,
+        fes_carbon_costs,
     )
 
     # Calculate average marginal cost across the years
@@ -162,7 +150,7 @@ if __name__ == "__main__":
     start_year = int(Path(snakemake.input.bid_offer_data[0]).stem)
     end_year = int(Path(snakemake.input.bid_offer_data[-1]).stem)
 
-    df_cost = calculate_marginal_costs(
+    df_cost = calculate_costs(
         fes_power_costs_path=snakemake.input.fes_power_costs,
         fes_carbon_costs_path=snakemake.input.fes_carbon_costs,
         fes_scenario=snakemake.params.fes_scenario,
